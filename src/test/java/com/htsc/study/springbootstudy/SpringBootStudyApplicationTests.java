0c8feee2225ac0c1ba0a29cd0a8c41f6f51ac87a
Zhuwangkun <2516973447@qq.com>
2024-07-19 15:58:00 +0800

    springboot study

---
+++ src/test/java/com/htsc/study/springbootstudy/SpringBootStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.htsc.study.springbootstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
